e2327c11206c29215725f2542d59cc2eb858bd66
ale3andro <ale3andro@gmail.com>
2016-06-06 15:36:17 +0300

    Ολοκλήρωση λειτουργίας αλλαγής σειράς actions

--- lab_organizr.py
+++ lab_organizr.py
@@ -52,7 +52,6 @@ class labOrganizr:
         self.school_classes = self.settings['general']['school_classes'].split(',')
         #Window1 widgets
         self.w1_treeview_columns = ["id", "Όνομα"]
-        self.w1_treeview_columns_selected = ["α/α", "Όνομα"]
 
         self.w1_treeview_available_modules = self.builder.get_object("w1_treeview_available_modules")
         self.w1_treeview_available_modules_liststore = Gtk.ListStore(int, str)
@@ -69,11 +68,10 @@ class labOrganizr:
             self.actions.append(dict(id=item['id'], name=item['name'], file=item['file']))
 
         self.w1_treeview_selected_modules = self.builder.get_object("w1_treeview_selected_modules")
-        self.w1_treeview_selected_modules_liststore = Gtk.ListStore(int, str, int)
-        for i in range(2):
-            cell = Gtk.CellRendererText()
-            col = Gtk.TreeViewColumn(self.w1_treeview_columns_selected[i], cell, text=i)
-            self.w1_treeview_selected_modules.append_column(col)
+        self.w1_treeview_selected_modules_liststore = Gtk.ListStore(str, int)
+        cell = Gtk.CellRendererText()
+        col = Gtk.TreeViewColumn("´Ονομα", cell, text=0)
+        self.w1_treeview_selected_modules.append_column(col)
         self.w1_treeview_selected_modules.set_model(self.w1_treeview_selected_modules_liststore)
 
         self.w1_treeview_select_pcs = self.builder.get_object("w1_treeview_select_pcs")
@@ -87,8 +85,6 @@ class labOrganizr:
             for item in self.settings['pcs']:
                 self.w1_treeview_select_pcs_liststore.append([item['id'], item['ip']])            
         
-        self.w1_button_move_up = self.builder.get_object("w1_button_move_up")
-        self.w1_button_move_down = self.builder.get_object("w1_button_move_down")
         self.w1_button_move_right = self.builder.get_object("w1_button_move_right")
         self.w1_button_move_left = self.builder.get_object("w1_button_move_left")
         self.w1_button_select_pcs_all = self.builder.get_object("w1_button_select_pcs_all")
@@ -164,36 +160,14 @@ class labOrganizr:
         if (len(self.w1_treeview_selected_modules_liststore)==0):
             return
         selection = self.w1_treeview_selected_modules.get_selection().get_selected()
-        numSelectedModules = len(self.w1_treeview_selected_modules_liststore) # Συνολικός αριθμός επιλεγμένων modules
-        numSelectedModule = selection[0].get_value(selection[1],0) # Το α/α του module που θα διαγραφεί
         if (selection[1]!=None):
-            for item in self.w1_treeview_selected_modules_liststore:
-                if (item[0]>=numSelectedModule):
-                    item[0] -= 1
             self.w1_treeview_selected_modules_liststore.remove(selection[1])
 
     def on_w1_button_move_right_clicked(self, *args):
         selection = self.w1_treeview_available_modules.get_selection().get_selected()
-        numSelectedModules = len(self.w1_treeview_selected_modules_liststore)
         if (selection[1]!=None):
-            item = [numSelectedModules+1, selection[0].get_value(selection[1],1), selection[0].get_value(selection[1],0)]
+            item = [selection[0].get_value(selection[1],1), selection[0].get_value(selection[1],0)]
             self.w1_treeview_selected_modules_liststore.append(item)
-
-    def on_w1_button_move_up_clicked(self, *args):
-        if (len(self.w1_treeview_selected_modules_liststore) == 0):
-            return
-        selection = self.w1_treeview_selected_modules.get_selection().get_selected()
-        if (selection[1] != None):
-            print "Move up!"
-            return
-
-    def on_w1_button_move_down_clicked(self, *args):
-        if (len(self.w1_treeview_selected_modules_liststore) == 0):
-            return
-        selection = self.w1_treeview_selected_modules.get_selection().get_selected()
-        if (selection[1] != None):
-            print "Move down!"
-            return
         
     def on_w1_button_select_pcs_all_clicked(self, *args):
         self.w1_treeview_select_pcs.get_selection().select_all()
@@ -225,7 +199,9 @@ class labOrganizr:
             return
         else:
             for item in self.w1_treeview_selected_modules_liststore:
-                self.selectedActions.append(item[2])
+                self.selectedActions.append(item[1])
+                print item[0], item[1]
+        return
         self.makeConnections()
 
     def getClientDetailsFromClassId(self, classId):
